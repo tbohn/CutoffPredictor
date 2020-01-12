Fixed bugs in date_diff computation.

backend/prep_features.py
@@ -338,8 +338,9 @@ def create_feature_table(df_occupant, df_location, df_meter, df_cutoffs,
                         .loc[((df_cutoffs['occupant_id'] == occ) &
                               (df_cutoffs['cutoff_at'] > today)), 'cutoff_at'] \
                         .values()[0]
-                    date_diff = ((date_first_cut - today) / \
-                                 np.timedelta64(1, 'M')).round().astype(int)
+                    date_first_cut = pd.to_datetime(date_first_cut)
+                    date_diff = int(np.round(((date_first_cut - today) / \
+                                              np.timedelta64(1, 'M')))
                     if date_diff <= nSamples:
                         label_tri = 2
                     else: